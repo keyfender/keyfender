274975327fbbbada1927a2cfce41f063664684ca
Sven Anderson <sven@anderson.de>
2016-07-06 16:02:05 +0200

    Make static ip easily configurable

--- src/config.ml
+++ src/config.ml
@@ -4,6 +4,13 @@ open Mirage
    FS=crunch (or nothing): use static filesystem via crunch
    FS=fat: use FAT and block device (run ./make-fat-images.sh)
  *)
+
+let address addr nm gw =
+  let f = Ipaddr.V4.of_string_exn in
+  { address = f addr ; netmask = f nm ; gateways = [f gw] }
+
+let static_ip_conf = address "192.168.2.5" "255.255.255.0" "192.168.2.1"
+
 let mode =
   try match String.lowercase (Unix.getenv "FS") with
     | "fat"     -> `Fat
@@ -36,7 +43,7 @@ let dhcp =
 let stack console =
   match net, dhcp with
   | `Direct, true  -> direct_stackv4_with_dhcp console tap0
-  | `Direct, false -> direct_stackv4_with_default_ipv4 console tap0
+  | `Direct, false -> direct_stackv4_with_static_ipv4 console tap0 static_ip_conf
   | `Socket, _     -> socket_stackv4 console [Ipaddr.V4.any]
 
 let http_srv = http_server (conduit_direct ~tls:true (stack default_console))
