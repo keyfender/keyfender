6271ff8e7fdad0e390fe02edec836ae5cd97d798
Jan Suhr <jan@suhr.org>
2016-07-07 21:34:49 +0200

    Performance tests fixed (but deactivated)

--- tests/end-to-end/src/test/scala/com/nitrokey/nethsmtest/NetHsmTest.scala
+++ tests/end-to-end/src/test/scala/com/nitrokey/nethsmtest/NetHsmTest.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import Crypto._
 import org.scalatest.FeatureSpec
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.IntegrationPatience
 import org.scalatest.time.{Span, Seconds, Millis}
 import com.typesafe.scalalogging.LazyLogging
 import spray.http._
@@ -22,7 +23,11 @@ import java.security.Security
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.openssl.PEMReader
 
-class NetHsmTest extends FeatureSpec with LazyLogging with ScalaFutures {
+/**
+ * These tests are highly dependent and all together represent an entire lifecycle of a NetHSM.
+ * In case of error, focus on the first error because it may cause subsequent errors.
+ */
+class NetHsmTest extends FeatureSpec with LazyLogging with ScalaFutures with IntegrationPatience {
 
   //Load settings
   //TODO: Handle IOException
@@ -38,13 +43,19 @@ class NetHsmTest extends FeatureSpec with LazyLogging with ScalaFutures {
   //Define the timeout which needs to be longer than default for remote web service calls
   //and generation of 4096 bit keys.
   val timeout = 10.seconds
-  implicit val defaultPatience =
-    PatienceConfig(timeout = Span(10, Seconds), interval = Span(100, Millis))
+  //implicit val defaultPatience =
+  //  PatienceConfig(timeout = Span(10, Seconds), interval = Span(100, Millis))
 
   //Define values for the test scenarios
   val rand = new scala.util.Random
+  
+  //These key lengths are going to be used for key generation
   val keyLengths = List(1024, 2048, 3072, 4096)
+  
+  //These hash algorithms are used in conjunction with signing
   val hashAlgorithms = List("md5", "sha1", "sha224", "sha256", "sha384", "sha512")
+  
+  //This list keeps all keys being generated and imported which are used for most subsequent tests
   val keyEnvelopes = new ListBuffer[PublicKeyEnvelope]
   val tempAdminPassword = "bla"
   val adminPassword = "super secret"
@@ -563,71 +574,94 @@ class NetHsmTest extends FeatureSpec with LazyLogging with ScalaFutures {
   }
 
   feature("Performance tests") {
-    val publicKey = null //keyEnvelopes.head.key.publicKey
-    val keyLocation = null //keyEnvelopes.head.location
-    //TODO generate new key
-
     ignore("Sign 100 messages (RSA, PKCS#1 padding)") {
       val rounds = 100
 
-      //Create 100 requests
-      val requests = (0 to rounds).map(counter => {
-        SignRequest( s"Secure your digital life $counter".getBytes )
-      })
-
-      //val pipeline = sendReceive
-
-      //create 100 response-futures
-      val responsesF = (0 to rounds).map(counter => {
-        val pipeline: HttpRequest => Future[HttpResponse] = (
-          addCredentials(BasicHttpCredentials("user", userPassword))
-          ~> sendReceive
-        )
-        pipeline(Post(s"$host$keyLocation/actions/pkcs1/sign", requests.seq(counter)))
-      })
-
-      //execute
-      val responses = time { Future.sequence(responsesF).futureValue }
-
-      //test
-      responses.map(response =>
-        assert(response.status.isSuccess)
+      //Generate key for this performance test
+      val request = KeyGeneration("signing", "RSA", 2048)
+      val pipeline: HttpRequest => Future[HttpResponse] = (
+        addCredentials(BasicHttpCredentials("admin", adminPassword))
+        ~> sendReceive
       )
+      val responseF: Future[HttpResponse] = pipeline(Post(s"$apiLocation/keys", request))
+      whenReady(responseF) { response =>
+        assert(response.status.intValue === 303) // Redirection to imported key
+        val location = response.headers.toList.filter(_.is("location"))
+        assert( location.nonEmpty )
+        val keyLocation = location.map(_.value).head
+    
+        //Create 100 requests before executing those
+        val requests = (0 to rounds).map(counter => {
+          SignRequest( s"Secure your digital life $counter".getBytes )
+        })
+  
+        //create 100 response-futures
+        val responsesF = (0 to rounds).map(counter => {
+          val pipeline: HttpRequest => Future[HttpResponse] = (
+            addCredentials(BasicHttpCredentials("user", userPassword))
+            ~> sendReceive
+          )
+          pipeline(Post(s"$host$keyLocation/actions/pkcs1/sign", requests.seq(counter)))
+        })
+  
+        //execute
+        val responses = time { Future.sequence(responsesF).futureValue }
+  
+        //test
+        responses.map(response =>
+          assert(response.status.isSuccess)
+        )
+      }
     }
-
+    
     ignore("Decrypt 100 messages (RSA, PKCS#1 padding)") {
       val rounds = 100
 
-      //Create 100 messages
-      val messages = (0 to rounds).map(counter => {
-        s"Secure your digital life $counter".getBytes
-      })
-
-      //Create 100 requests
-      val requests = (0 to rounds).map(counter => {
-        DecryptRequest( encrypt(messages(counter), "RSA/NONE/PKCS1Padding", publicKey) )
-      })
-
-      val pipeline = (
-        addCredentials(BasicHttpCredentials("user", userPassword))
+      //Generate key for this performance test
+      val keyPair = generateRSACrtKeyPair(2048)
+      val publicKey = keyPair.publicKey
+      val request = KeyImport("signing", "RSA", keyPair.privateKey)
+      val pipeline: HttpRequest => Future[HttpResponse] = (
+        addCredentials(BasicHttpCredentials("admin", adminPassword))
         ~> sendReceive
-        ~> unmarshal[DecryptResponse]
       )
-      //create 100 response-futures
-      val responsesF = (0 to rounds).map(counter => {
-        pipeline(Post(s"$host$keyLocation/actions/pkcs1/decrypt", requests.seq(counter)))
-      })
-
-      //execute
-      val responses = time { Future.sequence(responsesF).futureValue }
-
-      //test
-      responses.zipWithIndex.map{case (response, i) => {
-        assert(response.status == "ok")
-        assert(trimPrefix(response.data.decrypted) === messages(i) )
-      }}
+      val responseF: Future[HttpResponse] = pipeline(Post(s"$apiLocation/keys", request))
+      whenReady(responseF) { response =>
+        assert(response.status.intValue === 303) // Redirection to imported key
+        val location = response.headers.toList.filter(_.is("location"))
+        assert( location.nonEmpty )
+        val keyLocation = location.map(_.value).head
+  
+        //Create 100 messages
+        val messages = (0 to rounds).map(counter => {
+          s"Secure your digital life $counter".getBytes
+        })
+  
+        //Create 100 requests
+        val requests = (0 to rounds).map(counter => {
+          DecryptRequest( encrypt(messages(counter), "RSA/NONE/PKCS1Padding", publicKey) )
+        })
+  
+        val pipeline = (
+          addCredentials(BasicHttpCredentials("user", userPassword))
+          ~> sendReceive
+          ~> unmarshal[DecryptResponse]
+        )
+        //create 100 response-futures
+        val responsesF = (0 to rounds).map(counter => {
+          pipeline(Post(s"$host$keyLocation/actions/pkcs1/decrypt", requests.seq(counter)))
+        })
+  
+        //execute
+        val responses = time { Future.sequence(responsesF).futureValue }
+  
+        //test
+        responses.zipWithIndex.map{case (response, i) => {
+          assert(response.status == "success")
+          assert(trimPrefix(response.data.decrypted) === messages(i) )
+        }}
+      }
     }
-
   }
 
   scenario("Void admin password as preparation for next test run.") {
@@ -709,7 +743,9 @@ class NetHsmTest extends FeatureSpec with LazyLogging with ScalaFutures {
     val t0 = System.nanoTime()
     val result = block    // call-by-name
     val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0) + "ns = " + (t1 - t0)/1000000 + "ms")
+    info("Elapsed time: " + (t1 - t0) + "ns = " + (t1 - t0)/1000000 + "ms = " + (t1 - t0)/1000000000 + "s")
+    info("Time per operation: " + (t1 - t0)/100 + "ns = " + (t1 - t0)/100000000 + "ms")
+    info(1.0/((t1 - t0)/1000000000.0)*100.0 + " operations per second")
     result
   }
 }
