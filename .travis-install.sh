fd95f9a06345b99b061bbeb4e041773dabb0bc19
Sven Anderson <sven@anderson.de>
2017-05-30 02:29:49 +0200

    Replace CI pinning with version limits in config.ml

--- .travis-install.sh
+++ .travis-install.sh
@@ -9,15 +9,27 @@ opam init
 eval $(opam config env)
 opam switch $(opam switch | grep Official.*release | tail -1 | awk '{ print $3; }')
 eval $(opam config env)
-opam update -u
 
-# install mirage
-opam install mirage
-opam upgrade mirage
+# remove all pinnings from cache
+opam pin -n remove $(opam pin list -s)
+
+# 2.0.0 is incompatible
+# https://github.com/mirage/shared-memory-ring/issues/32
+opam pin -n add shared-memory-ring 1.3.0
 
 # webmachine 0.4.0 requires calendar which is not compatible with
 # mirage
-opam pin add webmachine 0.3.2
+# https://github.com/inhabitedtype/ocaml-webmachine/issues/73
+opam pin -n add webmachine 0.3.2
+
+# install mirage
+opam install mirage
+
+# bring cache up-to-date
+opam update
+
+# make sure we are in a stable state
+opam upgrade --fixup
 
 # install sbt
 mkdir $HOME/bin
