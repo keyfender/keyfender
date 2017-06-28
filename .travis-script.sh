5f6fa07859d21b30c11947e7c73a51acf7d60dd8
Sven Anderson <sven@anderson.de>
2017-06-28 03:25:51 +0200

    Container uses direct mode (Mirage TCP/IP stack)

--- .travis-script.sh
+++ .travis-script.sh
@@ -12,7 +12,7 @@ MODE=unix NET=direct make configure depend build
 docker/build.sh
 
 # run nethsm on port 4433
-docker run -i --rm -p4433:4433 nethsm/nethsm &
+docker run -i --rm --device=/dev/net/tun:/dev/net/tun --cap-add=NET_ADMIN -p4433:4433 nethsm/nethsm &
 
 # functional tests
 cd tests/end-to-end/
--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -ex
 
 docker build -f src/Dockerfile.build -t nethsm_builder src
-docker rm -f nethsm_tmp && true
+docker rm -f nethsm_tmp >/dev/null 2>&1 && true
 docker run -t --name nethsm_tmp nethsm_builder
-docker cp nethsm_tmp:/src/_build/main.native docker/nethsm
+docker cp nethsm_tmp:/src/_build/main.native docker/adds/nethsm
 docker rm nethsm_tmp
 docker build -f docker/Dockerfile.run -t nethsm/nethsm docker
-rm docker/nethsm
