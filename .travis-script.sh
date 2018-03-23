77068c0e9677f6fbd25749e40f7e30dfdad4cf02
Sven Anderson <sven@anderson.de>
2018-03-23 16:43:31 +0100

    Make irmin test server less verbose

--- .travis-script.sh
+++ .travis-script.sh
@@ -8,7 +8,7 @@ export BUILD_UI=1
 docker/build-all-in-one.sh
 
 docker network create -d bridge --subnet 10.99.99.0/24 --gateway 10.99.99.1 dockernet
-docker run -i --rm --net dockernet --ip 10.99.99.3 --name irmin -v $OPAM_DIR:/home/opam/.opam keyfender_buildbase irmin init -a http://0.0.0.0:8081 -d --verbosity=debug -s mem 2>&1 | sed "s/^/<irmin> /" &
+docker run -i --rm --net dockernet --ip 10.99.99.3 --name irmin -v $OPAM_DIR:/home/opam/.opam keyfender_buildbase irmin init -a http://0.0.0.0:8081 -d --verbosity=info -s mem 2>&1 | sed "s/^/<irmin> /" &
 
 # run keyfender on port 4433
 docker run -i --rm -p4433:4433 --device=/dev/net/tun:/dev/net/tun --cap-add=NET_ADMIN --net dockernet --ip 10.99.99.2 keyfender/keyfender --irmin http://10.99.99.3:8081 2>&1 | sed "s/^/<keyfender> /" &
