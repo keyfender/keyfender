a371526a624dc5bb0389417ccd23d6c63608ae8d
Sven Anderson <sven@anderson.de>
2018-05-15 16:12:18 +0200

    Add gateway and nameserver config to docker start script

--- docker/container-scripts/start.sh
+++ docker/container-scripts/start.sh
@@ -6,6 +6,8 @@ export MIRAGE_LOGS=${MIRAGE_LOGS:-debug}
 
 if [ -e /dev/net/tun ] ; then
   IP=$(ip -o -f inet address show eth0 | awk '{print $4}')
+  GW=$(ip -o -f inet route list | grep "^default via" | head -1 | awk '{print $3}')
+  NS=$(grep ^nameserver /etc/resolv.conf | head -1 | awk '{print $2}')
   ip address delete $IP dev eth0
   tunctl -t tap0
   brctl addbr br0
@@ -16,10 +18,10 @@ if [ -e /dev/net/tun ] ; then
 
   if [ -e /dev/kvm ] ; then
     echo "##### Starting keyfender as kvm instance #####"
-    /ukvm-bin --net=tap0 /keyfender.ukvm --ipv4 $IP $ARGS
+    /ukvm-bin --net=tap0 /keyfender.ukvm --ipv4=$IP --ipv4-gateway=$GW --nameserver=$NS $ARGS
   else
     echo "##### Starting keyfender with direct network #####"
-    /keyfender.direct --interface tap0 --ipv4 $IP $ARGS
+    /keyfender.direct --interface tap0 --ipv4 $IP --ipv4-gateway=$GW --nameserver=$NS $ARGS
   fi
 else
   echo "##### Starting keyfender on unix socket #####"
