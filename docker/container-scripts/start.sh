defc54558d1b7991c2029251a7a206b7df2800b2
Sven Anderson <sven@anderson.de>
2018-07-12 17:45:51 +0200

    Run keyfender as non-root user in container

--- docker/container-scripts/start.sh
+++ docker/container-scripts/start.sh
@@ -12,11 +12,13 @@ echo "##### Keyfender container setup #####"
 
 NS=${KEYFENDER_NS:-$(grep ^nameserver /etc/resolv.conf | head -1 | awk '{print $2}')}
 echo "Nameserver: $NS"
+ARGS="$ARGS --nameserver=$NS"
 
 KEYFENDER_MEM=${KEYFENDER_MEM:-32}
 
 export MIRAGE_LOGS=${MIRAGE_LOGS:-debug}
 echo "Log level: $MIRAGE_LOGS"
+ARGS="$ARGS --logs=$MIRAGE_LOGS"
 
 if [ -e /dev/net/tun ] ; then
   echo "Setting up tap0 device"
@@ -25,21 +27,23 @@ if [ -e /dev/net/tun ] ; then
   GW=$(ip -o -f inet route list | grep "^default via" | head -1 | awk '{print $3}')
   echo "Detected gateway: $GW"
   ip address delete $IP dev eth0
-  tunctl -t tap0
+  tunctl -t tap0 -u keyfender
   brctl addbr br0
   brctl addif br0 eth0
   brctl addif br0 tap0
   ifconfig br0 up
   ifconfig tap0 up
 
+  ARGS="$ARGS --ipv4=$IP --ipv4-gateway=$GW"
+
   if [ -e /dev/kvm ] ; then
     echo "##### Starting keyfender as kvm instance #####"
-    /ukvm-bin --net=tap0 --mem=$KEYFENDER_MEM /keyfender.ukvm --ipv4=$IP --ipv4-gateway=$GW --nameserver=$NS --logs=$MIRAGE_LOGS $ARGS
+    exec su -l keyfender -c "/ukvm-bin --net=tap0 --mem=$KEYFENDER_MEM /keyfender.ukvm $ARGS"
   else
     echo "##### Starting keyfender with direct network #####"
-    /keyfender.direct --interface tap0 --ipv4 $IP --ipv4-gateway=$GW --nameserver=$NS $ARGS
+    exec su -l keyfender -c "/keyfender.direct --interface tap0 $ARGS"
   fi
 else
   echo "##### Starting keyfender on unix socket #####"
-  /keyfender.socket --nameserver=$NS $ARGS
+  exec su -l keyfender -c "/keyfender.socket $ARGS"
 fi
